11ab394cf231933e74d54c20f5956668fccd2f71
Ömer Atayilmaz <64100323+OmerAtayilmaz@users.noreply.github.com>
2021-06-13 17:26:31 +0300

    resnposive nav

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ document.querySelector('.navbar-nav').addEventListener('click',function(e){
         if(id!=="#")document.querySelector(id).scrollIntoView({behavior:'smooth'});
     }
 });
+
+//
+const signUpButton=document.getElementById('signUp');
+const signInButton=document.getElementById('signIn');
+const container=document.getElementById('container');
+
+
+signUpButton.addEventListener('click',()=>container.classList.add('right-panel-active'));
+signInButton.addEventListener('click',()=>container.classList.remove('right-panel-active'));
