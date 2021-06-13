b3c30993e8e97ec56c14d8d9265891499f564d67
Ömer Atayilmaz <64100323+OmerAtayilmaz@users.noreply.github.com>
2021-06-13 15:16:24 +0300

    resnposive nav

--- main.js
+++ main.js
@@ -3,10 +3,8 @@
 //scroll
 document.querySelector('.navbar-nav').addEventListener('click',function(e){
     e.preventDefault();
-
-    console.log(e.target.getAttribute('href'));
     if(e.target.classList.contains('nav-link')){
-        const id=e.target.getAttribute('href');
+       const  id=e.target.getAttribute('href');
         if(id!=="#")document.querySelector(id).scrollIntoView({behavior:'smooth'});
     }
 });
